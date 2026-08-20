973034f44ccf8370b3fc636b3c309a5ba4b3bf42
SaadMushtaqqqq <saadunraja2345@gmail.com>
2026-08-20 13:17:28 +0500

    Vercel fix

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -30,7 +30,7 @@ const LoginForm = () => {
     setResponseMsg('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- src/components/Signupform/Signupform.jsx
+++ src/components/Signupform/Signupform.jsx
@@ -23,7 +23,7 @@ const SignupForm = ({ onBackToLogin }) => {
     setResponseMsg('');
 
     try {
-      const response = await fetch('http://localhost:5000/api/register', {
+      const response = await fetch('/api/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
